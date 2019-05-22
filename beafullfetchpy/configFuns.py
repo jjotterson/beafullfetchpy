a08ae434450f4f29365c9c85f3b3687c96c1cb4e
James Otterson <James.Otterson@cbo.gov>
2019-05-22 15:43:49 -0400

    GUI is now its own package - fixed path when loading bff internal jsons

--- beafullfetchpy/configFuns.py
+++ beafullfetchpy/configFuns.py
@@ -1,5 +1,5 @@
 import json
-
+import pkg_resources
 
 def getKey(beaEntryName = 'BEA'):
     path = (getConfig())['ApiKeysPath']
@@ -11,17 +11,29 @@ def getKey(beaEntryName = 'BEA'):
         print('Could not retrive key of ' + beaEntryName + ' from \n '+path)
         pass   
 
+def getResourcePath(relativePath, resource_package = __name__):
+    '''
+     Given relative, get its full path
+     eg: relative path: /config/userSettings.json
+     will return
+     beafullfetchpy path + relative path
+     note: can replace resource_package with package name:
+     eg: 'beafullfetchpy'
+    '''
+    fullPath = pkg_resources.resource_filename(resource_package, relativePath)
+    return(fullPath)
 
 def getConfig():
     '''
         loads the configuration file.
     '''
+    userSettingsPath = getResourcePath('/config/userSettings.json')
     try:
-        with open('beafullfetchpy/config/userSettings.json') as jsonFile:
+        with open(userSettingsPath) as jsonFile:
              config = json.load(jsonFile)
         return(config)
     except:
-        print('Could not open the configuration file: \n beafullfetchpy/config/userSettings.json')
+        print('beafullfetchpy/configFuns: Could not open the configuration file: \n beafullfetchpy/config/userSettings.json')
         pass
 
 
@@ -29,8 +41,9 @@ def setConfig_apiKeyPath(newPath):
     '''
        sets the api key path in the package config file
     '''
+    userSettingsPath = getResourcePath('/config/userSettings.json')
     try:
-        with open('beafullfetchpy/config/userSettings.json') as jsonFile:
+        with open(userSettingsPath) as jsonFile:
              config = json.load(jsonFile)
     except:
         print('Could not open the configuration file: \n beafullfetchpy/config/userSettings.json')
@@ -39,7 +52,7 @@ def setConfig_apiKeyPath(newPath):
     config['ApiKeysPath'] = newPath
 
     try:
-        with open('beafullfetchpy/config/userSettings.json','w') as jsonFile:
+        with open(userSettingsPath,'w') as jsonFile:
             json.dump(config,jsonFile)
         print('Path to the API Keys updated! New Path: \n' + config['ApiKeysPath'])
     except:
