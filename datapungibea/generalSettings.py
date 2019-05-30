596ffbbbf0000d06b2c3fdf7b985f17824f3dc15
James Otterson <James.Otterson@cbo.gov>
2019-05-30 18:58:48 -0400

    refactoring: putting the code in generic format

---
+++ datapungibea/generalSettings.py
@@ -0,0 +1,101 @@
+'''
+  datapungi.generalSettings
+  ~~~~~~~~~~~~~~~~~~
+
+  General information on the data source (with disclaimer for accuracy), and 
+  general information on the package itself - this is to be passed to 
+  a GUI.  Also, this gets updated to include information on the 
+  individual methods used to get databases of the datasource
+'''
+
+class generalInformationAndSettings():
+    def __init__(self,userConfig={}):
+        ''' 
+         __datasourceOverview - a quick description of the datasource and its license
+         __packageMetadata - basic info on the package - to be used in a GUI or catalog of 
+            methods that read data.  Also, "databases" will get automaticall updated with
+            info on the methods that get specific dataset from the datasource.  A typical 
+            entry should look like:
+            {
+                 "displayName":"List of Datasets",
+                 "method"     :"datasetlist",   #NOTE run with getattr(data,'datasetlist')()
+                 "params"     :{},              #No parameters in this case.
+            }
+        '''
+        
+        self.__datasourceOverview = '''
+         Userguides:
+          https://apps.bea.gov/api/_pdf/bea_web_service_api_user_guide.pdf
+          https://www.bea.gov/tools/   or  https://apps.bea.gov/API/signup/index.cfm
+         
+          Basically, there are three types of meta: 
+            (1) GETDATASETLIST      top level, get the name of all tables.  
+            (2) GetParameterList    given a table, what parameters it needs to download (eg. NIPA)
+            (3) GetParameterValues  given a parameter of a table, which values you can choose. (eg. TableID)
+         
+         Sample python code (getting the list of datasets):
+         
+            import requests 
+            payload = {
+                'UserID':  ENTER YOUR BEA API Key Here, 
+                'method': 'GETDATASETLIST',
+                'ResultFormat': "JSON"
+            }
+            beaDatasets = requests.get( 'https://apps.bea.gov/api/data/', params = payload )
+        
+         Licenses (always check with the data provider):
+            Data used is sourced from the Bureau of Economic Analysis 
+            As stated on the Bureau of Economic Analysis website: 
+            - Unless stated otherwise, information published on this site is in the public 
+               domain and may be used or reproduced without specific permission. 
+            - As a U.S. government agency, BEA does not endorse or recommend any 
+              commercial products or services.                                            
+            - Any reference or link to the BEA Web site must not contain information 
+              that suggests an endorsement or recommendation by BEA.                  
+            For more information, see: 
+             https://www.bea.gov/help/guidelines-for-citing-bea  
+        '''   
+           
+        self.__packageMetadata = {
+            "name":             "datapungibea",
+            "loadPackageAs" :   "dpbea",
+            "apiClass":         "data",
+            "displayName":      "BEA",
+            "description":      "Acess data from Bureau of Economic Analysis (BEA)",
+            "databases":        [
+                {
+                 "displayName":"List of Datasets",
+                 "method"     :"datasetlist",   #NOTE run with getattr(data,'datasetlist')()
+                 "params"     :{},
+                },
+                {
+                 "displayName":"NIPA",
+                 "method"     :"NIPA",   #NOTE run with getattr(data,'datasetlist')()
+                 "params"     :{'Year':'X','Frequency':'Q'}, #Parameters and default options.
+                },
+                ],
+         }              
+           
+    
+    def __getSessionParameters(self,config={}):
+        pass
+
+
+
+
+
+'''
+
+                {
+                 "displayName":"List of Datasets",
+                 "method"     :"datasetlist",   #NOTE run with getattr(data,'datasetlist')()
+                 "params"     :{},
+                },
+                {
+                 "displayName":"NIPA",
+                 "method"     :"NIPA",   #NOTE run with getattr(data,'datasetlist')()
+                 "params"     :{'Year':'X','Frequency':'Q'}, #Parameters and default options.
+                },
+             ],
+        }
+'''
